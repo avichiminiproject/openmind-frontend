422d9fe0f784274f0e1db883c06a62bda25706c1
avichiminiproject <avichiminiproject@outlook.com>
2024-08-14 13:33:11 +0530

    header added

--- src/home/components/Header.jsx
+++ src/home/components/Header.jsx
@@ -4,9 +4,13 @@ const Header = () => {
   return (
     <>
     <header className='bg-litegrey'>
-        <img src="" alt="" />
-        <nav>
-            naveen
+        <nav className='flex items-center justify-between px-3 py-2'>
+            <img src="./src/assets/images/home/header_logo.png" alt="header logo" className='h-16 p-2' />
+            <ul className='text-white flex items-center justify-between gap-5 font-semibold'>
+                <li>About</li>
+                <li>Service</li>
+                <li>Contact</li>
+            </ul> 
         </nav>
     </header>
     </>
