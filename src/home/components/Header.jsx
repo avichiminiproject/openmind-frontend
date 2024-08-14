bb8d7329b540142a2ec77bcbc627557a467571a7
avichiminiproject <avichiminiproject@outlook.com>
2024-08-14 14:07:10 +0530

    a tag added

--- src/home/components/Header.jsx
+++ src/home/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <>
     <header className='bg-litegrey'>
         <nav className='flex items-center justify-between px-3 py-2'>
-            <img src="./src/assets/images/home/header_logo.png" alt="header logo" className='h-16 p-2' />
+            <a href={'/'}><img src="./src/assets/images/home/header_logo.png" alt="header logo" className='h-16 p-2' /></a>
             <ul className='text-white flex items-center justify-between gap-5 font-semibold'>
                 <li>
                 <Link to={'/about'}>About</Link>
